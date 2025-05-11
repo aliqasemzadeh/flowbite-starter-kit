a3ed1a94dcdc34a4fcf953e25ba7dbec2ebd3d01
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-11 10:28:55 +0330

    Replace email with username for user identification
    Updated the User model, migration, and registration logic to replace email with username as the primary identifier. Adjusted nullable constraints for certain fields to improve flexibility, and updated dependencies to include GIF and captcha-related libraries.

--- app/Livewire/Guest/Auth/Register.php
+++ app/Livewire/Guest/Auth/Register.php
@@ -14,7 +14,7 @@ class Register extends Component
 {
     public string $first_name = '';
     public string $last_name = '';
-    public string $email = '';
+    public string $username = '';
     public string $password = '';
     public string $password_confirmation = '';
     /**
@@ -25,7 +25,7 @@ class Register extends Component
         $validated = $this->validate([
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'username' => ['required', 'string', 'lowercase', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
         ]);
 
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('username')->unique();
-            $table->string('mobile')->unique();
-            $table->string('national_code')->unique();
+            $table->string('mobile')->unique()->nullable();
+            $table->string('national_code')->unique()->nullable();
             $table->string('password');
-            $table->string('email')->unique();
-            $table->dateTime('birth_at')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->dateTime('birth_at')->nullable()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('mobile_verified_at')->nullable();
             $table->rememberToken();
