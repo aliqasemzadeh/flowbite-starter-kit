e248c37de79fcb30de7f321f9f90e529c5efa82e
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-11 10:16:52 +0330

    Add mobile field and new index to users table migration
    Added a unique mobile field with a corresponding mobile_verified_at timestamp. Replaced the previous index with a new one for first_name and last_name to optimize user search functionality.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,15 +16,17 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('username')->unique();
+            $table->string('mobile')->unique();
             $table->string('national_code')->unique();
             $table->string('password');
             $table->string('email')->unique();
             $table->dateTime('birth_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('mobile_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
-            $table->index(['username', 'national_code', 'email']);
+            $table->index(['first_name', 'last_name'], 'user_search_index');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
