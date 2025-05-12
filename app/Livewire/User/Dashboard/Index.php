da1ebe6406468d88376e5353d95a4e886da59e98
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-12 18:20:55 +0330

    Update panel layout with new header and additional features
    The panel layout now includes a revamped header with a Flowbite

--- app/Livewire/User/Dashboard/Index.php
+++ app/Livewire/User/Dashboard/Index.php
@@ -7,7 +7,6 @@ use Livewire\Component;
 
 class Index extends Component
 {
-    #[Layout('components.layouts.panel')]
     public function render()
     {
         return view('livewire.user.dashboard.index');
