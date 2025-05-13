f8585695b5803efda2948a24e524d30155c505d6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-13 10:31:23 +0330

    Update dashboard view and add authenticated route group
    Replaced placeholder text in the user dashboard view with "Hello World" for better clarity. Added a new authenticated route group and defined a route for the user dashboard index page to ensure proper access control.

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', App\Livewire\Landing\Index::class)->name('home');
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/user/dashboard/index', function () {})->name('user.dashboard.index');
+});
+
 require __DIR__.'/auth.php';
