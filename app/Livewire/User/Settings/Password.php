ca77824c4fc307a4b792f8c5bae29cd99271723c
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-13 11:02:17 +0330

    Update profile view and apply layout attribute to components
    Replaced placeholder text in the profile view with a styled login form, enhancing the UI. Added the `#[Layout]` attribute

--- app/Livewire/User/Settings/Password.php
+++ app/Livewire/User/Settings/Password.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire\User\Settings;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Password extends Component
 {
+
+    #[Layout('components.layouts.panel')]
     public function render()
     {
         return view('livewire.user.settings.password');
--- app/Livewire/User/Settings/Profile.php
+++ app/Livewire/User/Settings/Profile.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\User\Settings;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Profile extends Component
 {
+    #[Layout('components.layouts.panel')]
     public function render()
     {
         return view('livewire.user.settings.profile');
