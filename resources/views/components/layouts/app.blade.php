bf90cf80d12eef17b4a6448397390a9494f66400
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-13 09:54:14 +0330

    Add global notifications component to layout files
    Included the <x-notifications /> component in panel, auth, and app layout files to ensure consistent display of notifications across different sections of the application. This improves user feedback and enhances the overall user experience.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,7 @@
     @include('partials.head')
 </head>
 <body>
+<x-notifications />
     <!-- Navigation Bar -->
     <header>
         <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
