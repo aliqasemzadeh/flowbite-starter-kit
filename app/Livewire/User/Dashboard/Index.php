08b57f5d517adea9260fb5528de6dccceba07158
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-13 10:18:56 +0330

    Add layout attribute to user dashboard component
    This change applies the `#[Layout]` attribute to specify the layout for the user dashboard component. It ensures consistent rendering by using the predefined `components.layouts.panel` layout.

--- app/Livewire/User/Dashboard/Index.php
+++ app/Livewire/User/Dashboard/Index.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire\User\Dashboard;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+
+    #[Layout('components.layouts.panel')]
     public function render()
     {
         return view('livewire.user.dashboard.index');
