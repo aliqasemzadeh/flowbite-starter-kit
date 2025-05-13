8b6aac5b7c9cee832757d6062e7ff0dac392cc95
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-13 11:11:45 +0330

    Add user profile management and email verification
    Introduce functionality to view and update user profile information, including name and email, with validation. Implement email verification resend capability and enhance user feedback with notifications.

--- resources/views/partials/user.blade.php
+++ resources/views/partials/user.blade.php
@@ -552,7 +552,7 @@
                 <a
                     href="{{ route('user.settings.profile') }}"
                     class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
-                >{{ __('kit.profile_information') }}</a
+                >{{ __('kit.profile') }}</a
                 >
             </li>
             <li>
