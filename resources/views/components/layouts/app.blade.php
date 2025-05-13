ece9cf3ff5455e213a12131da69255a7aac74f8b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-13 09:54:41 +0330

    Set notification position to bottom-end in layouts.
    Updated the notification component across panel, auth, and app layouts to display notifications at the bottom-end of the screen. This improves consistency and better aligns with the overall UI design.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
     @include('partials.head')
 </head>
 <body>
-<x-notifications />
+<x-notifications position="bottom-end" />
     <!-- Navigation Bar -->
     <header>
         <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
--- resources/views/components/layouts/panel.blade.php
+++ resources/views/components/layouts/panel.blade.php
@@ -4,7 +4,7 @@
     @include('partials.head')
 </head>
 <body>
-<x-notifications />
+<x-notifications position="bottom-end" />
 <div class="antialiased bg-gray-50 dark:bg-gray-900">
     <nav class="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
         <div class="flex flex-wrap justify-between items-center">
