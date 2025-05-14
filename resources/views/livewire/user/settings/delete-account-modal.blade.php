8fa28325b1277a3b5931656dc2ae4c27ad38de7a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-15 03:12:29 +0330

    Add delete-account modal and update Laravel dependencies
    Created a dedicated modal for account deletion, moving the related UI from the profile view to its own component. Updated Laravel Framework and Sail dependencies to their latest versions for improved stability and features.

---
+++ resources/views/livewire/user/settings/delete-account-modal.blade.php
@@ -0,0 +1,23 @@
+<x-livewire-modal::stack>
+    <x-livewire-modal::slideover
+        position="center"
+        class="w-full max-w-md overflow-auto rounded-lg bg-white p-5"
+    >
+        <x-card title="Consent Terms">
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+
+            <x-slot name="footer" class="flex justify-end gap-x-4">
+                <x-button flat label="Cancel" x-on:click="close" />
+
+                <x-button primary label="I Agree" wire:click="agree" />
+            </x-slot>
+        </x-card>
+    </x-livewire-modal::slideover>
+</x-livewire-modal::stack>
--- resources/views/livewire/user/settings/profile.blade.php
+++ resources/views/livewire/user/settings/profile.blade.php
@@ -2,22 +2,7 @@
     <x-button label="Open" x-on:click="$openModal('simpleModal')" primary />
 
     <x-modal name="simpleModal">
-        <x-card title="Consent Terms">
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
 
-            <x-slot name="footer" class="flex justify-end gap-x-4">
-                <x-button flat label="Cancel" x-on:click="close" />
-
-                <x-button primary label="I Agree" wire:click="agree" />
-            </x-slot>
-        </x-card>
     </x-modal>
     <div class="w-full p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
         <h5 class="text-xl font-medium text-gray-900 dark:text-white">{{ __('kit.profile') }}</h5>
@@ -54,7 +39,6 @@
     <div class="w-full p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mt-6">
         <h5 class="text-xl font-medium text-gray-900 dark:text-white">{{ __('kit.delete_account') }}</h5>
         <x-button label="{{ __('kit.delete') }}" x-on:click="$openModal('deleteAccountDialog')" negative />
-
         <x-modal name="deleteAccountDialog" bordered>
             <form wire:submit="deleteUser" class="space-y-6">
                 <x-card title="{{ __('kit.delete_account') }}">
@@ -74,6 +58,13 @@
                 </x-card>
             </form>
         </x-modal>
+        <x-button warning label="Delete"
+                  x-on:confirm="{
+    title: 'Sure Delete?',
+    icon: 'warning',
+    method: 'delete',
+    params: 1
+}" />
     </div>
 
 </div>
