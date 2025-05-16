fc77649bad709f5deeae3ee4b935bfa58d207ebd
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-16 16:09:46 +0330

    Update password form to improve clarity and usability
    Revised labels and placeholders for better user understanding. Added explicit handling for current, new, and confirmation passwords to align with form semantics. Updated button label to maintain consistency with the form's purpose.

--- resources/views/livewire/user/settings/password.blade.php
+++ resources/views/livewire/user/settings/password.blade.php
@@ -1,32 +1,32 @@
 <div class="p-4 h-auto pt-10">
     <div class="w-full p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <h5 class="text-xl font-medium text-gray-900 dark:text-white">{{ __('kit.update_password') }}</h5>
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white">{{ __('kit.change_password') }}</h5>
 
         <form class="space-y-4 md:space-y-6" wire:submit="updatePassword">
 
             <x-password
+                wire:model="current_password"
+                :label="__('Current password')"
+                type="password"
                 required
-                wire:model="password"
                 autocomplete="current-password"
-                label="{{ __('kit.password') }}"
-                placeholder="{{ __('kit.password') }}"
             />
-
             <x-password
-                required
                 wire:model="password"
-                label="{{ __('kit.password') }}"
-                placeholder="{{ __('kit.password') }}"
-            />
-
-            <x-password
+                :label="__('New password')"
+                type="password"
                 required
+                autocomplete="new-password"
+            />
+            <x-password
                 wire:model="password_confirmation"
-                label="{{ __('kit.password_confirmation') }}"
-                placeholder="{{ __('kit.password_confirmation') }}"
+                :label="__('Confirm Password')"
+                type="password"
+                required
+                autocomplete="new-password"
             />
 
-            <x-button primary type="submit" label="{{ __('kit.update') }}"/>
+            <x-button primary type="submit" label="{{ __('kit.change_password') }}"/>
         </form>
     </div>
 </div>
