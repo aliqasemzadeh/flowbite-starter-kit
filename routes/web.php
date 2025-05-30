5dff14eca596471cf77a2c5abfb94f4ad74a8940
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-30 16:43:14 +0330

    Remove unused demo views, routes, and dependencies. Add new package dependencies (`spatie/laravel-package-tools` and `wire-elements/modal`).

--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,6 @@ Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/user/settings/profile', \App\Livewire\User\Settings\Profile::class)->name('user.settings.profile');
     Route::get('/user/settings/password', \App\Livewire\User\Settings\Password::class)->name('user.settings.password');
-
-    // Test route for file input component
-    Route::get('/file-input-test', function () {
-        return view('file-input-test');
-    })->name('file-input-test');
-
-    // Test route for checkbox component
-    Route::get('/checkbox-demo', function () {
-        return view('examples.checkbox-demo');
-    })->name('checkbox-demo');
-
-    // Test route for pin code component
-    Route::get('/pin-code-example', function () {
-        return view('examples.pin-code-example');
-    })->name('pin-code-example');
 });
 
 require __DIR__.'/auth.php';
